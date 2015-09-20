6659164e5a3cac977c4af0f42a8c74e7e27c3405
yasyas001 <515274057@qq.com>
2015-09-20 17:14:42 +0800

    提交二次

---
+++ com-bj-childOne/src/main/java/com/bj/childOne/App.java
@@ -0,0 +1,14 @@
+package com.bj.childOne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("修改文件");
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com-bj-childTwo/src/main/java/org/com/bj/childTwo/App.java
@@ -0,0 +1,13 @@
+package org.com.bj.childTwo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
